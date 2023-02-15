d5575efd9617b3b25f058f6efeee90e68326430e
kianah10 <kharri10@chicagobooth.edu>
2023-02-15 05:22:32 +0000

    11:22

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc })
     render ({:template => "users_templates/index.html.erb"})
   end
+
+  def show
+    #Parameters: {"path_username"=>"anisa"}
+    url_username = params.fetch("path_username")
+
+    matching_usernames = User.where({ :username=> url_username})
+
+    @the_user = matching_usernames.at(0)
+    
+    render({ :template => "users_templates/show.html.erb"})
+  end
 end
